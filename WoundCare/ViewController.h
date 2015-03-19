6b87c61d0047c1ea125e4f9d768cb7889fd2c111
Sangeetha <Sangeetha@Sangeethas-Mac-mini.local>
2015-03-19 11:08:02 +0530

    Initial Commit

---
+++ WoundCare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WoundCare
+//
+//  Created by Sangeetha on 19/03/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WoundCare/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WoundCare
+//
+//  Created by Sangeetha on 19/03/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
