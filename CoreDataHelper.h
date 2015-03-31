4e28bd9ab2c9975db77de52cb01d72e5d7b624e3
Sujitha <sujitha@Sujithas-Mac-mini.local>
2015-03-31 13:14:07 +0530

    sujitha

--- CoreDataHelper.h
+++ CoreDataHelper.h
@@ -4,7 +4,7 @@
 //
 //  Created by Sangeetha on 16/03/15.
 //  Copyright (c) 2015 Sangeetha. All rights reserved.
-//
+//sujitha - check git chnages
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
